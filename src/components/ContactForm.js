729964c717113fed38c9b5f6f1081330fd31b16a
Louise Kristensen <louiseetkristensen@gmail.com>
2024-10-24 15:03:50 +0200

    ContactForm

---
+++ src/components/ContactForm.js
@@ -0,0 +1,117 @@
+function ContactInfo() {
+
+    const Info = [{
+        firstname: (''), 
+        lastname: (''), 
+        email: (''), 
+        phone: (''), 
+        company: (''), 
+        position: (''), 
+    }]
+
+
+    const newContact = (
+        firstname, 
+        lastname, 
+        email, 
+        phone, 
+        company, 
+        position
+    )
+
+};
+
+    return (
+        <section>
+            <div>
+                <div>
+                    <form onSubmit={submitForm}>
+                    <h2>Kontaktoplysninger</h2>
+                    <div>
+                        <label>
+                            Fornavn 
+                        </label>
+                        <input
+                            type='text'
+                            id='firstname'
+                            name='firstname'
+                            required
+                            value={firstname}
+                            onChange={(e) => setFirstname(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <label>
+                            Efternavn 
+                        </label>
+                        <input
+                            type='text'
+                            id='lastname'
+                            name='lastname'
+                            required
+                            value={lastname}
+                            onChange={(e) => setLastname(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <label>
+                            Email 
+                        </label>
+                        <input
+                            type='text'
+                            id='email'
+                            name='email'
+                            required
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <label>
+                            Telefonnummer  
+                        </label>
+                        <input
+                            type='text'
+                            id='phone'
+                            name='phone'
+                            required
+                            value={phone}
+                            onChange={(e) => setPhone(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <label>
+                            Firma 
+                        </label>
+                        <input
+                            type='text'
+                            id='company'
+                            name='company'
+                            value={company}
+                            onChange={(e) => setComapny(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <label>
+                            Stilling 
+                        </label>
+                        <input
+                            type='text'
+                            id='position'
+                            name='position'
+                            value={position}
+                            onChange={(e) => setPosition(e.target.value)}
+                        />
+                    </div>
+
+                    <div>
+                        <button type='submit'>Tilføj kontakt</button>
+                    </div>
+                    </form>
+                </div>
+            </div>
+        </section>
+    );
+
+export default ContactForm;
+
