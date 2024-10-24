934ba941e93ecedbce563f3cb08c8d7a5bb463cf
Louise Kristensen <louiseetkristensen@gmail.com>
2024-10-24 13:45:29 +0200

    modal

--- src/components/Pip.js
+++ src/components/Pip.js
@@ -1,13 +1,9 @@
-import React from 'react';
 
-function Pip({ avatar, username, message }) {
+function Pip({username, message }) {
   return (
     <div className="pip-message">
-      <img className="profilepictureFeed" src={avatar} alt={username} />
-      <div>
         <div className="username-display"><strong>{username}</strong></div>
         <div className="message-display">{message}</div>
-      </div>
     </div>
   );
 }
