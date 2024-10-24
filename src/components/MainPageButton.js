82948375373d6d8f3cc32bdaa64d8c76ff31bcd2
Louise Kristensen <louiseetkristensen@gmail.com>
2024-10-24 12:06:30 +0200

    mainPageButton

---
+++ src/components/MainPageButton.js
@@ -0,0 +1,11 @@
+function MainPageButton(props) {
+    return (
+        <div>
+            <button>
+                <link>{props.message}</link>
+            </button>
+        </div>
+    )
+}
+
+export default MainPageButton
