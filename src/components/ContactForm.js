aacf1ae2f758d4421fdc57c429381b86ce7f6f9c
Louise Kristensen <louiseetkristensen@gmail.com>
2024-10-29 09:46:24 +0100

    ContactForm

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,118 +1,155 @@
-// function ContactInfo() {
+import { useState } from "react";
 
-//     const Info = [{
-//         firstname: (''), 
-//         lastname: (''), 
-//         email: (''), 
-//         phone: (''), 
-//         company: (''), 
-//         position: (''), 
-//     }]
+function ContactForm() {
+    const [firstname, setFirstname] = useState('');
+    const [lastname, setLastname] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [company, setCompany] = useState('');
+    const [position, setPosition] = useState('');
+    const [error, setError] = useState(null); 
 
 
-//     // const newContact = (
-//     //     firstname, 
-//     //     lastname, 
-//     //     email, 
-//     //     phone, 
-//     //     company, 
-//     //     position
-//     // )
+    const handleOnContactChange = (e) => {
+        const { name, value } = e.target;
     
+        switch (name) {
+            case 'firstname':
+              setFirstname(value);
+              break;
+            case 'lastname':
+              setLastname(value);
+              break;
+            case 'email':
+              setEmail(value);
+              break;
+            case 'phone':
+              setPhone(value);
+              break;
+            case 'company':
+              setCompany(value);
+              break;
+            case 'position':
+              setPosition(value);
+              break;
+            default:
+              break;
+          }
+        };
 
+        function handleAddContact(e) {
+            if (firstname === '' || lastname === '' || email === '') {
+                setError('Cannot add empty field.');
+              } else {
+                setError(null);
+                console.log("Contact added:", { firstname, lastname, email, phone, company, position });
+                setFirstname('');
+                setLastname('');
+                setEmail('');
+                setPhone('');
+                setCompany('');
+                setPosition('');
+              }
+            }
 
-//     return (
-//         <section>
-//             <div>
-//                 <div>
-//                     <form onSubmit={submitForm}>
-//                     <h2>Kontaktoplysninger</h2>
-//                     <div>
-//                         <label>
-//                             Fornavn 
-//                         </label>
-//                         <input
-//                             type='text'
-//                             id='firstname'
-//                             name='firstname'
-//                             required
-//                             value={firstname}
-//                             onChange={(e) => setFirstname(e.target.value)}
-//                         />
-//                     </div>
-//                     <div>
-//                         <label>
-//                             Efternavn 
-//                         </label>
-//                         <input
-//                             type='text'
-//                             id='lastname'
-//                             name='lastname'
-//                             required
-//                             value={lastname}
-//                             onChange={(e) => setLastname(e.target.value)}
-//                         />
-//                     </div>
-//                     <div>
-//                         <label>
-//                             Email 
-//                         </label>
-//                         <input
-//                             type='text'
-//                             id='email'
-//                             name='email'
-//                             required
-//                             value={email}
-//                             onChange={(e) => setEmail(e.target.value)}
-//                         />
-//                     </div>
-//                     <div>
-//                         <label>
-//                             Telefonnummer  
-//                         </label>
-//                         <input
-//                             type='text'
-//                             id='phone'
-//                             name='phone'
-//                             required
-//                             value={phone}
-//                             onChange={(e) => setPhone(e.target.value)}
-//                         />
-//                     </div>
-//                     <div>
-//                         <label>
-//                             Firma 
-//                         </label>
-//                         <input
-//                             type='text'
-//                             id='company'
-//                             name='company'
-//                             value={company}
-//                             onChange={(e) => setComapny(e.target.value)}
-//                         />
-//                     </div>
-//                     <div>
-//                         <label>
-//                             Stilling 
-//                         </label>
-//                         <input
-//                             type='text'
-//                             id='position'
-//                             name='position'
-//                             value={position}
-//                             onChange={(e) => setPosition(e.target.value)}
-//                         />
-//                     </div>
+    return (
+        <section>
+            <div>
+                <div>
+                    <form>
+                    <h2>Kontaktoplysninger</h2>
+                    <div>
+                        <label>
+                            Fornavn 
+                        </label>
+                        <input
+                            type='text'
+                            id='firstname'
+                            name='firstname'
+                            placeholder='Fornavn'
+                            required
+                            value={firstname}
+                            onChange={handleOnContactChange}
+                        />
+                    </div>
+                    <div>
+                        <label>
+                            Efternavn 
+                        </label>
+                        <input
+                            type='text'
+                            id='lastname'
+                            name='lastname'
+                            placeholder='Efternavn'
+                            required
+                            value={lastname}
+                            onChange={handleOnContactChange}
+                        />
+                    </div>
+                    <div>
+                        <label>
+                            Email 
+                        </label>
+                        <input
+                            type='text'
+                            id='email'
+                            name='email'
+                            placeholder='Email'
+                            required
+                            value={email}
+                            onChange={handleOnContactChange}
+                        />
+                    </div>
+                    <div>
+                        <label>
+                            Telefonnummer  
+                        </label>
+                        <input
+                            type='text'
+                            id='phone'
+                            name='phone'
+                            placeholder='Telefonnummer'
+                            required
+                            value={phone}
+                            onChange={handleOnContactChange}
+                        />
+                    </div>
+                    <div>
+                        <label>
+                            Firma 
+                        </label>
+                        <input
+                            type='text'
+                            id='company'
+                            name='company'
+                            placeholder='Firma'
+                            value={company}
+                            onChange={handleOnContactChange}
+                        />
+                    </div>
+                    <div>
+                        <label>
+                            Stilling 
+                        </label>
+                        <input
+                            type='text'
+                            id='position'
+                            name='position'
+                            placeholder='Stilling'
+                            value={position}
+                            onChange={handleOnContactChange}
+                        />
+                    </div>
 
-//                     <div>
-//                         <button type='submit'>Tilføj kontakt</button>
-//                     </div>
-//                     </form>
-//                 </div>
-//             </div>
-//         </section>
-//     );
-// }
+                    <div>
+                        <button type='submit' onClick={handleAddContact}>Tilføj kontakt</button>
+                    </div>
+                    </form>
+                </div>
+            </div>
+        </section>
+    );
+}
 
-// export default ContactForm;
+export default ContactForm;
 
